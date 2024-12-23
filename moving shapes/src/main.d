3027e85c4a8f1ac6a8bab3eb651d8ca8ac95b9b0
K-M-Shehan <miweshshehan@gmail.com>
2024-12-23 11:14:28 +0530

    added the skeleton code

---
+++ moving shapes/src/main.d
@@ -0,0 +1,6 @@
+src/main.o: src/main.cpp src/imgui/imgui.h src/imgui/imconfig.h \
+ src/imgui/imgui-SFML.h src/imgui/imgui-SFML_export.h
+src/imgui/imgui.h:
+src/imgui/imconfig.h:
+src/imgui/imgui-SFML.h:
+src/imgui/imgui-SFML_export.h:
